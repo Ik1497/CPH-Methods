d9624574d0d33bfc1f807473145ba6d08d8f3b65
Ik <113038466+Ik1497@users.noreply.github.com>
2023-06-25 12:37:35 +0200

    Update VTubeStudio.js

--- composables/Methods/VTubeStudio.js
+++ composables/Methods/VTubeStudio.js
@@ -1,3 +1,104 @@
 export default {
+  ////////////
+  // Models //
+  ////////////
 
+  VTubeStudioLoadModelById: {
+    title: `Load model by id`,
+    description: `Load a VTube Studio model by its id.`,
+    version: `0.2.0`,
+    tags: [`Models`],
+    return: `bool`,
+    fields: [
+      {
+        datatype: `string`,
+        name: `modelId`
+      },
+    ]
+  },
+  VTubeStudioLoadModelByName: {
+    title: `Load model by name`,
+    description: `Load a VTube Studio model by its name.`,
+    version: `0.2.0`,
+    tags: [`Models`],
+    return: `bool`,
+    fields: [
+      {
+        datatype: `string`,
+        name: `modelName`
+      },
+    ]
+  },
+  VTubeStudioMoveModel: {
+    title: `Move model`,
+    description: `Move the current VTube Studio model.`,
+    version: `0.2.0`,
+    tags: [`Models`],
+    return: `bool`,
+    fields: [
+      {
+        datatype: `double`,
+        name: `seconds`
+      },
+      {
+        datatype: `bool`,
+        name: `relative`
+      },
+      {
+        datatype: `double`,
+        nullable: true,
+        name: `posX`
+        default: `null`,
+      },
+      {
+        datatype: `double`,
+        nullable: true,
+        name: `posY`
+        default: `null`,
+      },
+      {
+        datatype: `double`,
+        nullable: true,
+        name: `rotation`
+        default: `null`,
+      },
+      {
+        datatype: `double`,
+        nullable: true,
+        name: `size`
+        default: `null`,
+      },
+    ]
+  },
+
+  /////////////
+  // Hotkeys //
+  /////////////
+
+  VTubeStudioTriggerHotkeyById: {
+    title: `Trigger hotkey by id`,
+    description: `Trigger a VTube Studio hotkey by its id.`,
+    version: `0.2.0`,
+    tags: [`Hotkeys`],
+    return: `bool`,
+    fields: [
+      {
+        datatype: `string`,
+        name: `hotkeyId`
+      },
+    ]
+  },
+  VTubeStudioTriggerHotkeyByName: {
+    title: `Trigger hotkey by name`,
+    description: `Trigger a VTube Studio hotkey by its name.`,
+    version: `0.2.0`,
+    tags: [`Hotkeys`],
+    return: `bool`,
+    fields: [
+      {
+        datatype: `string`,
+        name: `hotkeyName`
+      },
+    ]
+  },
 }
